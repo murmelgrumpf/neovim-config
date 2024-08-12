634b77dfbb857a409ad5f0c5c122d170187d7347
jan.gippert@u2d.de <jan.gippert@u2d.de>
2024-08-12 17:45:43 +0200

    Contect

---
+++ lua/plugins/context.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require('treesitter-context').setup({
+
+                separator = '~'
+            })
+        end
+    }
+}
