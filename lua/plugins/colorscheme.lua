3b8e33126892828f8c115a13de41a712a6f97281
jan.gippert@u2d.de <jan.gippert@u2d.de>
2024-08-13 12:52:20 +0200

    better line nubers

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,9 @@ return {
         priority = 1000,
         config = function()
             vim.cmd('colorscheme tokyonight-storm')
+            vim.cmd('highlight LineNr guifg=#b6bef0')
+            vim.cmd('highlight LineNrBelow guifg=#9aa1cc')
+            vim.cmd('highlight LineNrAbove guifg=#9aa1cc')
         end
     }
 }
