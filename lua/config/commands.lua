19252640c09d88803b3d5a6eef2d71dc7bdf93d3
Murmel Gippert <jan.gippert@u2d.de>
2024-09-10 12:17:32 +0200

    PE

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -3,3 +3,4 @@ vim.api.nvim_create_user_command('W', "execute ':silent w !sudo tee % > /dev/nul
 vim.api.nvim_create_user_command('SS', ':set list!', {})
 vim.api.nvim_create_user_command('PS', ':%s/    /	/g', {})
 vim.api.nvim_create_user_command('PT', ':%s/	/    /g', {})
+vim.api.nvim_create_user_command('PE', ':silent! %s/\\n\\s*\\n\\(\\s*\\)}/\\r\\1}/g', {})
